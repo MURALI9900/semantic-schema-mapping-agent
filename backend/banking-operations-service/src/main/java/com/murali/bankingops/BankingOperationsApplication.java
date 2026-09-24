05f9a44ae2f02b5685bc92162288edf9b6c14761
MURALI9900 <90686104+MURALI9900@users.noreply.github.com>
2026-09-24 15:30:31 +0530

    feat: add banking operations application

---
+++ backend/banking-operations-service/src/main/java/com/murali/bankingops/BankingOperationsApplication.java
@@ -0,0 +1,5 @@
+package com.murali.bankingops;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BankingOperationsApplication { public static void main(String[] args){SpringApplication.run(BankingOperationsApplication.class,args);} }
