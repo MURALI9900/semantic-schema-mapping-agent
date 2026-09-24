342b82d1a41a6cfe2bafb53023dd270423ef9ee5
MURALI9900 <90686104+MURALI9900@users.noreply.github.com>
2026-09-24 15:30:13 +0530

    feat: add audit service application

---
+++ backend/audit-service/src/main/java/com/murali/auditservice/AuditServiceApplication.java
@@ -0,0 +1,5 @@
+package com.murali.auditservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AuditServiceApplication { public static void main(String[] args){SpringApplication.run(AuditServiceApplication.class,args);} }
